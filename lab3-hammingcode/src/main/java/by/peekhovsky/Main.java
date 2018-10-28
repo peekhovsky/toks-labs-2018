6ea413a6f410060525d9acc08eefb09a59a334c6
peekhovsky <peekhovsky@gmail.com>
2018-10-28 14:53:57 +0300

    commit

---
+++ lab3-hammingcode/src/main/java/by/peekhovsky/Main.java
@@ -0,0 +1,138 @@
+package by.peekhovsky;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main  {
+
+    synchronized static void print(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+
+        boolean portIsOpened = false;
+        Scanner scanner = new Scanner(System.in);
+
+        MessengerCore messengerCore = new MessengerCore();
+
+        String baudName = "9600";
+        String portName;
+
+        while (true) {
+
+            ArrayList<String> portNames = messengerCore.getPortNames();
+
+            if (portNames.isEmpty()) {
+                print("There is no ports in your computer.");
+                return;
+            } else {
+                portName = portNames.get(0);
+            }
+
+
+            while (!portIsOpened) {
+
+                print("1 - Connect, 2 - Change port, 3 - Change baud, 0 - Exit");
+                print("Port: " + portName);
+                print("Baud: " + baudName);
+
+                int t;
+                try {
+
+                    t = scanner.nextInt();
+                } catch (InputMismatchException e) {
+                    print("Wrong input!");
+                    scanner.next();
+                    continue;
+                }
+
+                switch (t) {
+                    case 1:
+                        if (messengerCore.connect(portName, baudName)) {
+                            portIsOpened = true;
+                        }
+                        break;
+                    case 2: {
+                        print("Available ports: " + portNames);
+                        print("Print a name of port: ");
+                        String s = scanner.next();
+                        if (portNames.contains(s)) {
+                            portName = s;
+                            print("Port name has been changed.");
+                        } else {
+                            print("Wrong name of a port!");
+                        }
+                        break;
+                    }
+                    case 3: {
+                        print("Available bauds: " + Arrays.toString(MessengerCore.speeds));
+                        print("Print a baud: ");
+                        String s = scanner.next();
+                        if (Arrays.binarySearch(MessengerCore.speeds, s) != -1) {
+                            baudName = s;
+                            print("The baud has been changed.");
+                        } else {
+                            print("Wrong name of a baud!");
+                        }
+                        break;
+                    }
+                    case 0:
+                        return;
+
+                }
+            }
+
+
+            while (portIsOpened) {
+                print("1 - Print message, 2 - Close port, 3 - Send a message with mistake, 0 - Exit");
+                int t;
+                try {
+                    t = scanner.nextInt();
+                } catch (InputMismatchException e) {
+                    print("Wrong input!");
+                    scanner.next();
+                    continue;
+                }
+                switch (t) {
+                    case 1: {
+                        scanner.reset();
+                        print("Print a message: ");
+
+                        String m = scanner.next();
+                        messengerCore.sendMessage(m, -1);
+                        break;
+                    }
+                    case 2: {
+                        portIsOpened = false;
+                        messengerCore.stop();
+                        break;
+                    }
+                    case 3: {
+                        scanner.reset();
+                        print("Print a message: ");
+                        String m = scanner.next();
+
+                        print("Set a position of mistake: ");
+                        Integer pos = scanner.nextInt();
+                        messengerCore.sendMessage(m, pos);
+
+                        break;
+                    }
+                    case 0:
+                        portIsOpened = false;
+                        if (messengerCore.stop()) {
+                            return;
+                        }
+                        break;
+                    default: {
+                        print("Wrong input!");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lab3-hammingcode/src/test/java/by/peekhovsky/StuffingTest.java
@@ -0,0 +1,39 @@
+package by.peekhovsky;
+
+import by.peekhovsky.ByteStuffing;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class StuffingTest {
+
+    @Test
+    public void testStuffing() {
+        byte[] bytes = "~~~".getBytes();
+        byte[] sufferedBytes = ByteStuffing.doStuffing(bytes);
+        byte[] injectedBytes = ByteStuffing.inject(sufferedBytes);
+
+        assertTrue(Arrays.equals(bytes, injectedBytes));
+
+        bytes = "}}rognd}}}}ojef}epff}}nrf".getBytes();
+        sufferedBytes = ByteStuffing.doStuffing(bytes);
+        injectedBytes = ByteStuffing.inject(sufferedBytes);
+
+        assertTrue(Arrays.equals(bytes, injectedBytes));
+
+        bytes = "ro~~~nd}}~}}ojef}~epff}~}nrf}}}}}}}}".getBytes();
+        sufferedBytes = ByteStuffing.doStuffing(bytes);
+        injectedBytes = ByteStuffing.inject(sufferedBytes);
+
+        assertTrue(Arrays.equals(bytes, injectedBytes));
+
+        bytes = " dnwdnwkjndjwndnwkjdnwdkjw~~~~~~~~~~~~~}}}}}}}}}}}}}~~~~~~~~~~~}}}}}}}}}}".getBytes();
+        sufferedBytes = ByteStuffing.doStuffing(bytes);
+        injectedBytes = ByteStuffing.inject(sufferedBytes);
+
+        assertTrue(Arrays.equals(bytes, injectedBytes));
+
+    }
+}
